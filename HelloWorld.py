11a3eb95430f62229da8bd84f7fbd132b86716d2
chopin83 <n.armstrong83000@gmail.com>
2022-01-13 09:43:10 +0100

    Add files via upload

---
+++ HelloWorld.py
@@ -0,0 +1,40 @@
+print("Python has three numeric types: int, float, and complex");
+myValue=1
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+myValue=3.14
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+myValue=5j
+print(myValue)
+print(type(myValue)),print(str(myValue) + " is of the data type " + str(type(myValue)))
+myValue=True
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+myValue=False
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+myString= "This is a string"
+print(myString)
+print(type(myString))
+print(myString + " is of the data type " + str(type(myString)))
+firstString = "water"
+secondString = "fall"
+thirdString = firstString + secondString
+print(thirdString)
+name = input("What is your name? ")
+print(name)
+color = input("What is your favorite color?  ")
+animal = input("What is your favorite animal?  ")
+print("{}, you like a {} {}!".format(name,color,animal))
+myFruitList = ["apple", "banana", "cherry"]
+print(myFruitList)
+print(type(myFruitList))
+print(myFruitList[0])
+print(myFruitList[1])
+print(myFruitList[2])
+myFruitList[2] = "orange"
---
+++ for-loop.py
@@ -0,0 +1,7 @@
+"""
+Your module description
+"""
+print("Count to 10!")
+for b in range (0, 11):
+    print(b)
+    
